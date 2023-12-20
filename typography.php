70fa9c4e099cfa9824ae67bdefeed4608a598791
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-20 04:28:15 +0100

    Typography update

--- typography.php
+++ typography.php
@@ -66,34 +66,81 @@
                 <small class="caption">.title-1</small>
                 <div class="type-demo-margin type-demo-margin-top"></div>
                 <div class="type-demo-padding type-demo-padding-top"></div>
-                <h1 class="title-1" style="margin-top:0;padding-top:0;">Polykit</h1>
+                <h1 class="title-1" style="margin-top:0;padding-top:0;margin-bottom:0;">Polykit Design System</h1>
                 <div class="type-demo-margin type-demo-margin-bottom"></div>
             </section>
+
+            <hr>
 
             <section class="heading-1-demo">
                 <small class="caption">.heading-1</small>
                 <div class="type-demo-margin type-demo-margin-top"></div>
                 <div class="type-demo-padding type-demo-padding-top"></div>
-                <h2 class="heading-1">An Open-source Design System</h2>
+                <h2 class="heading-1" style="margin-top:0;padding-top:0;margin-bottom:0;">An Open-source Design System for Web Development</h2>
                 <div class="type-demo-margin type-demo-margin-bottom"></div>
             </section>
 
+            <hr>
+
+            <section class="heading-2-demo">
                 <small class="caption">.heading-2</small>
-                <h3 class="heading-2">Seamless UX-Code Handoff</h3>
+                <div class="type-demo-margin type-demo-margin-top"></div>
+                <div class="type-demo-padding type-demo-padding-top"></div>
+                <h3 class="heading-2" style="margin-top:0;padding-top:0;margin-bottom:0;">Seamless UX-Code Handoff</h3>
+                <div class="type-demo-margin type-demo-margin-bottom"></div>
+            </section>
 
+            <hr>
+
+            <section class="heading-3-demo">
                 <small class="caption">.heading-3</small>
-                <h4 class="heading-3">Beautiful Typography</h4>
+                <div class="type-demo-margin type-demo-margin-top"></div>
+                <div class="type-demo-padding type-demo-padding-top"></div>
+                <h4 class="heading-3" style="margin-top:0;padding-top:0;margin-bottom:0;">Beautiful Typography</h4>
+                <div class="type-demo-margin type-demo-margin-bottom"></div>
+            </section>
 
+            <hr>
+
+            <section class="heading-4-demo">
                 <small class="caption">.heading-4</small>
-                <h5 class="heading-4">Customizable</h5>
+                <div class="type-demo-margin type-demo-margin-top"></div>
+                <div class="type-demo-padding type-demo-padding-top"></div>
+                <h5 class="heading-4" style="margin-top:0;padding-top:0;margin-bottom:0;">Customizable</h5>
+                <div class="type-demo-margin type-demo-margin-bottom"></div>
+            </section>
 
+            <hr>
+
+            <section class="heading-5-demo">
+                <small class="caption">.heading-5</small>
+                <div class="type-demo-margin type-demo-margin-top"></div>
+                <div class="type-demo-padding type-demo-padding-top"></div>
+                <h6 class="heading-5" style="margin-top:0;padding-top:0;margin-bottom:0;">Customizable</h6>
+                <div class="type-demo-margin type-demo-margin-bottom"></div>
+            </section>
+
+            <hr>
+
+            <section class="p-demo">
                 <small class="caption">&lt;p&gt;</small>
-                <p class="">Made with a focus on beautiful typography and seamless integration between ux design and code.</p>
+                <div class="type-demo-margin type-demo-margin-top"></div>
+                <div class="type-demo-padding type-demo-padding-top"></div>
+                <p class="" style="margin-top:0;padding-top:0;margin-bottom:0;">Made with a focus on beautiful typography and seamless integration between ux design and code.</p>
+                <div class="type-demo-margin type-demo-margin-bottom"></div>
+            </section>
 
+            <hr>
+
+            <section class="small-demo">
                 <small class="caption">&lt;small&gt;</small>
-                <small>Built by <a href="https://thepolyfengroup.com">The Polyfen Group</a></small>
+                <div class="type-demo-margin type-demo-margin-top"></div>
+                <div class="type-demo-padding type-demo-padding-top"></div>
+                <small style="margin-top:0;padding-top:0;margin-bottom:0;">Built by <a href="https://thepolyfengroup.com">The Polyfen Group</a></small>
+                <div class="type-demo-margin type-demo-margin-bottom"></div>
+            </section>
         
-                <hr>
+            <hr>
         </main>
         <footer>
             <a href="https://thepolyfengroup.com" target="_blank">
