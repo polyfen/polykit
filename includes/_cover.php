8a7a9a299d3e0519dc2f65a3fa98eedc4a7f6ffa
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-21 16:55:20 +0100

    Update _cover.php

--- includes/_cover.php
+++ includes/_cover.php
@@ -1,7 +1,7 @@
 <section class="align-center">
     <h1 class="title-1 align-center">An Open-source Design System</h1>
 
-    <p class="align-center">Made with a focus on beautiful typography and seamless integration between ux design and code.</p>
+    <p class="align-center">Made with a focus on beautiful typography and seamless integration between design and code.</p>
     
     <a class="button" href="https://github.com/polyfen/polykit" target="_blank">Github Repo</a>
 </section>
