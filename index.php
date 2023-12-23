5903d217bb23147b9f9e83cf085b4f37d8b90989
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-23 17:35:42 +0100

    Updated favicon

--- index.php
+++ index.php
@@ -15,9 +15,9 @@
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
         <link rel="manifest" href="/site.webmanifest">
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ffaa00">
-        <meta name="msapplication-TileColor" content="#ffaa00">
-        <meta name="theme-color" content="#ffaa00">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#1f8df0">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
 
         <!-- FONTS -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
