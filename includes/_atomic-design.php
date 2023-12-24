4c4d4438ae8c44fe7d096975987d700b0e251e54
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-24 17:21:22 +0100

    Update _atomic-design.php

--- includes/_atomic-design.php
+++ includes/_atomic-design.php
@@ -1,5 +1,5 @@
 <section>
-    <h2 class="heading-1">An Atomic Design System</h2>
+    <h2 class="heading-1">Built with Atomic Design</h2>
     <p>Our design system follows the rationale proposed by Brad Frost in a blog post called <a href="https://bradfrost.com/blog/post/atomic-web-design/">&quot;Atomic Web Design&quot;</a>.</p>
     <img src="imgs/site/atomic-design.png" alt="Atomic Design" width="100%">
 </section>
