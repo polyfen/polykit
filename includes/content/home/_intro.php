b7e7e6642ad1b88082f09776e90619997a976d06
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-26 17:14:22 +0100

    Added 2 col section to home

--- includes/content/home/_intro.php
+++ includes/content/home/_intro.php
@@ -1,21 +1,27 @@
-<section>
+<section class="product-features">
     <h2 class="heading-1 align-center">We ship CSS and Figma updates <strong>together</strong></h2>
-    <p>The code is:</p>
-    <ul>
-        <li>Light-weight</li>
-        <li>Mobile-first</li>
-        <li>W3C compliant</li>
-        <li>Following accessability best practices</li>
-        <li>Following SEO best practices</li>
-        <li>Easy to read</li>
-        <li>Easy to pick-up</li>
-        <li>Easy to maintain and scale</li>
-    </ul>
-    <p>The design is:</p>
-    <ul>
-        <li>Clean</li>
-        <li>Consistent</li>
-        <li>Easy to customize and scale</li>
-        <li>Following typography best practices</li>
-    </ul>
+    <div class="two-col">
+        <div>
+            <p style="margin-top: 1rem; margin-bottom: 0;">The code is</p>
+            <ul>
+                <li>Light-weight</li>
+                <li>Mobile-first</li>
+                <li>W3C compliant</li>
+                <li>Following accessability best practices</li>
+                <li>Following SEO best practices</li>
+                <li>Easy to read</li>
+                <li>Easy to pick-up</li>
+                <li>Easy to maintain and scale</li>
+            </ul>
+        </div>
+        <div>
+            <p style="margin-top: 1rem; margin-bottom: 0;">The design is</p>
+            <ul>
+                <li>Clean</li>
+                <li>Consistent</li>
+                <li>Easy to customize and scale</li>
+                <li>Following typography best practices</li>
+            </ul>
+        </div>
+    </div>
 </section>
