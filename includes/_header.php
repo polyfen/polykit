583ed61cecdfe96b4797ebc81bfcd09ec34d28d6
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-26 17:58:28 +0100

    small  display change

--- includes/_header.php
+++ includes/_header.php
@@ -2,7 +2,7 @@
     <a href="/">
         <img src="imgs/polykit-logo.svg" height="50px" width="auto">
     </a>
-    <div>
+    <div style="display:flex; gap:8px;">
         <small>v 0.1</small>
         <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>Roadmap</small></a>
     </div>
