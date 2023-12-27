5c01ee99c6d81fd882d40e282c3b1362714e04f8
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-27 15:41:17 +0100

    Update _install-css.php

--- includes/content/getting-started/_install-css.php
+++ includes/content/getting-started/_install-css.php
@@ -1,7 +1,6 @@
 <section>
     <h3 class="heading-2">Installing Polykit via CSS</h2>
     <p>Add the following line of code in your <code>&lt;head&gt;</code>.</p>
-    <pre><code>
-&lt;link <span class="hljs-keyword">rel</span>=<span class="hljs-string">"stylesheet"</span> href=<span class="hljs-string">"https://polykit.xyz/app/css/polykit-v0-1.css"</span> <span class="hljs-keyword">type</span>=<span class="hljs-string">"text/css"</span>&gt;
+    <pre><code>&lt;link <span class="hljs-keyword">rel</span>=<span class="hljs-string">"stylesheet"</span> href=<span class="hljs-string">"https://polykit.xyz/app/css/polykit-v0-1.css"</span> <span class="hljs-keyword">type</span>=<span class="hljs-string">"text/css"</span>&gt;
 </code></pre>
 </section>
