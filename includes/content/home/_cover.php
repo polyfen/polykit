d3b8ecf32c8fe65d6c66b8b2d5dd91ce7d9855b5
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-28 18:26:22 +0100

    Added Figma File button to Homepage

--- includes/content/home/_cover.php
+++ includes/content/home/_cover.php
@@ -4,4 +4,5 @@
     <p class="align-center">Made with a focus on beautiful typography and seamless integration between design and code.</p>
     
     <a class="button" href="https://github.com/polyfen/polykit" target="_blank"><i class="icon ph-fill ph-github-logo" style="margin-right:0.375rem;"></i> Github Repo</a>
+    <a class="button" href="https://www.figma.com/file/ljE12vilrDMg0r9vJfQLBz/Polykit-%7C-Design-System-%5Bv0.1%5D-%5BSTG%5D?type=design&node-id=84%3A51&mode=design&t=sdkwx6zC4JQrObhN-1" target="_blank"><i class="icon ph-fill ph-figma-logo" style="margin-right:0.375rem;"></i> Figma File</a>
 </section>
