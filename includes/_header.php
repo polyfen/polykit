3087074e7dfe437026790859b4327664bafa4684
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-30 22:52:34 +0100

    Added Donate link

--- includes/_header.php
+++ includes/_header.php
@@ -5,6 +5,7 @@
     <div style="display:flex; gap:8px;">
         <small>v 0.1</small>
         <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>Roadmap</small></a>
+        <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>Donate</small></a>
     </div>
     <nav>
         <a href="/">Home</a>
