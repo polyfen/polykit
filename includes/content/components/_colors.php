290558dcdbdc5a9d34e1ff9ff5896d3d419618fd
Boris Hrnčić <borishrncic@users.noreply.github.com>
2023-12-31 13:20:07 +0100

    Added Indigo color

--- includes/content/components/_colors.php
+++ includes/content/components/_colors.php
@@ -1,180 +1,350 @@
 <section>
     <h3 class="heading-1">Colors</h3>
-    <h4 class="heading-2">Blue</h4>
-    <div class="color-wrapper">
-        <div class="color-swatches-container">  
-            <div>
-                <div class="color-swatch blue-100"></div>
+    <p>For maximum control and precision of the color schemes all colors are defined with the HSL color system in the code.</p>
+    <section><!-- BLUE -->
+        <h4 class="heading-2">Blue</h4>
+        <div class="color-wrapper">
+            <div class="color-swatches-container">  
                 <div>
-                    $blue-100<br>
-                    #E6F0FF<br>
-                    hsl(214, 100%, 95%)
+                    <div class="color-swatch blue-100"></div>
+                    <div>
+                        $blue-100<br>
+                        #E6F0FF<br>
+                        hsl(214, 100%, 95%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-200"></div>
+                    <div>
+                        $blue-200<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-300"></div>
+                    <div>
+                        $blue-300<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-400"></div>
+                    <div>
+                        $blue-400<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-500"></div>
+                    <div>
+                        $blue-500<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-600"></div>
+                    <div>
+                        $blue-600<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-700"></div>
+                    <div>
+                        $blue-700<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-800"></div>
+                    <div>
+                        $blue-800<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-900"></div>
+                    <div>
+                        $blue-900<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
                 </div>
             </div>
-            <div>
-                <div class="color-swatch blue-200"></div>
+            <div class="color-swatches-container">  
                 <div>
-                    $blue-200<br>
-                    #<br>
-                    hsl(#, #%, #%)
+                    <div class="color-swatch blue-90pct"></div>
+                    <div>
+                        $blue-90pct<br>
+                        #E6F0FF5A<br>
+                        hsl(214, 100%, 95%, 0.9)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-80pct"></div>
+                    <div>
+                        $blue-80pct<br>
+                        #E6F0FFCC<br>
+                        hsl(214, 100%, 95%, 0.8)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-70pct"></div>
+                    <div>
+                        $blue-70pct<br>
+                        #E6F0FFB3<br>
+                        hsl(214, 100%, 95%, 0.7)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-60pct"></div>
+                    <div>
+                        $blue-60pct<br>
+                        #E6F0FF99<br>
+                        hsl(214, 100%, 95%, 0.6)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-50pct"></div>
+                    <div>
+                        $blue-50pct<br>
+                        #E6F0FF80<br>
+                        hsl(214, 100%, 95%, 0.5)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-40pct"></div>
+                    <div>
+                        $blue-40pct<br>
+                        #E6F0FF66<br>
+                        hsl(214, 100%, 95%, 0.4)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-30pct"></div>
+                    <div>
+                        $blue-30pct<br>
+                        #E6F0FF4D<br>
+                        hsl(214, 100%, 95%, 0.3)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-20pct"></div>
+                    <div>
+                        $blue-20pct<br>
+                        #E6F0FF33<br>
+                        hsl(214, 100%, 95%, 0.2)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch blue-10pct"></div>
+                    <div>
+                        $blue-10pct<br>
+                        #E6F0FF1A<br>
+                        hsl(214, 100%, 95%, 0.1)
+                    </div>
                 </div>
             </div>
-            <div>
-                <div class="color-swatch blue-300"></div>
+            <div class="color-swatches-container">  
                 <div>
-                    $blue-300<br>
-                    #<br>
-                    hsl(#, #%, #%)
+                    <div class="color-swatch blue-gradient"></div>
+                    <div>
+                        $blue-gradient<br>
+                        #E6F0FF<br>
+                        hsl(214, 100%, 95%)
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-400"></div>
                 <div>
-                    $blue-400<br>
-                    #<br>
-                    hsl(#, #%, #%)
+                    <div class="color-swatch blue-gradient-start"></div>
+                    <div>
+                        $blue-gradient-start<br>
+                        #E6F0FF<br>
+                        hsl(214, 100%, 95%)
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-500"></div>
                 <div>
-                    $blue-500<br>
-                    #<br>
-                    hsl(#, #%, #%)
-                </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-600"></div>
-                <div>
-                    $blue-600<br>
-                    #<br>
-                    hsl(#, #%, #%)
-                </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-700"></div>
-                <div>
-                    $blue-700<br>
-                    #<br>
-                    hsl(#, #%, #%)
-                </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-800"></div>
-                <div>
-                    $blue-800<br>
-                    #<br>
-                    hsl(#, #%, #%)
-                </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-900"></div>
-                <div>
-                    $blue-900<br>
-                    #<br>
-                    hsl(#, #%, #%)
+                    <div class="color-swatch blue-gradient-end"></div>
+                    <div>
+                        $blue-gradient-end<br>
+                        #E6F0FF<br>
+                        hsl(214, 100%, 95%)
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="color-swatches-container">  
-            <div>
-                <div class="color-swatch blue-90pct"></div>
+    </section>
+    <hr class="secondary-separator">
+    <section><!-- INDIGO -->
+        <h4 class="heading-2">Indigo</h4>
+        <div class="color-wrapper">
+            <div class="color-swatches-container">  
                 <div>
-                    $blue-90pct<br>
-                    #E6F0FF5A<br>
-                    hsl(214, 100%, 95%, 0.9)
+                    <div class="color-swatch indigo-100"></div>
+                    <div>
+                        $indigo-100<br>
+                        #E6F0FF<br>
+                        hsl(214, 100%, 95%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch indigo-200"></div>
+                    <div>
+                        $indigo-200<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                <div class="color-swatch indigo-300"></div>
+                <div>
+                    $indigo-300<br>
+                    #<br>
+                    hsl(#, #%, #%)
+                </div>
+                </div>
+                <div>
+                    <div class="color-swatch indigo-400"></div>
+                    <div>
+                        $indigo-400<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch indigo-500"></div>
+                    <div>
+                        $indigo-500<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch indigo-600"></div>
+                    <div>
+                        $indigo-600<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch indigo-700"></div>
+                    <div>
+                        $indigo-700<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch indigo-800"></div>
+                    <div>
+                        $indigo-800<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
+                </div>
+                <div>
+                    <div class="color-swatch indigo-900"></div>
+                    <div>
+                        $indigo-900<br>
+                        #<br>
+                        hsl(#, #%, #%)
+                    </div>
                 </div>
             </div>
-            <div>
-                <div class="color-swatch blue-80pct"></div>
+            <div class="color-swatches-container">  
                 <div>
-                    $blue-80pct<br>
+                    <div class="color-swatch indigo-90pct"></div>
+                    <div>
+                        $indigo-90pct<br>
+                        #E6F0FF5A<br>
+                        hsl(214, 100%, 95%, 0.9)
+                    </div>
+                </div>
+                <div>
+                <div class="color-swatch indigo-80pct"></div>
+                <div>
+                    $indigo-80pct<br>
                     #E6F0FFCC<br>
                     hsl(214, 100%, 95%, 0.8)
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-70pct"></div>
-                <div>
-                    $blue-70pct<br>
-                    #E6F0FFB3<br>
-                    hsl(214, 100%, 95%, 0.7)
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-60pct"></div>
                 <div>
-                    $blue-60pct<br>
-                    #E6F0FF99<br>
-                    hsl(214, 100%, 95%, 0.6)
+                    <div class="color-swatch indigo-70pct"></div>
+                    <div>
+                        $indigo-70pct<br>
+                        #E6F0FFB3<br>
+                        hsl(214, 100%, 95%, 0.7)
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-50pct"></div>
                 <div>
-                    $blue-50pct<br>
-                    #E6F0FF80<br>
-                    hsl(214, 100%, 95%, 0.5)
+                    <div class="color-swatch indigo-60pct"></div>
+                    <div>
+                        $indigo-60pct<br>
+                        #E6F0FF99<br>
+                        hsl(214, 100%, 95%, 0.6)
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-40pct"></div>
                 <div>
-                    $blue-40pct<br>
-                    #E6F0FF66<br>
-                    hsl(214, 100%, 95%, 0.4)
+                    <div class="color-swatch indigo-50pct"></div>
+                    <div>
+                        $indigo-50pct<br>
+                        #E6F0FF80<br>
+                        hsl(214, 100%, 95%, 0.5)
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-30pct"></div>
                 <div>
-                    $blue-30pct<br>
-                    #E6F0FF4D<br>
-                    hsl(214, 100%, 95%, 0.3)
+                    <div class="color-swatch indigo-40pct"></div>
+                    <div>
+                        $indigo-40pct<br>
+                        #E6F0FF66<br>
+                        hsl(214, 100%, 95%, 0.4)
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-20pct"></div>
                 <div>
-                    $blue-20pct<br>
-                    #E6F0FF33<br>
-                    hsl(214, 100%, 95%, 0.2)
+                    <div class="color-swatch indigo-30pct"></div>
+                    <div>
+                        $indigo-30pct<br>
+                        #E6F0FF4D<br>
+                        hsl(214, 100%, 95%, 0.3)
+                    </div>
                 </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-10pct"></div>
                 <div>
-                    $blue-10pct<br>
-                    #E6F0FF1A<br>
-                    hsl(214, 100%, 95%, 0.1)
+                    <div class="color-swatch indigo-20pct"></div>
+                    <div>
+                        $indigo-20pct<br>
+                        #E6F0FF33<br>
+                        hsl(214, 100%, 95%, 0.2)
+                    </div>
                 </div>
+                <div>
+                    <div class="color-swatch indigo-10pct"></div>
+                    <div>
+                        $indigo-10pct<br>
+                        #E6F0FF1A<br>
+                        hsl(214, 100%, 95%, 0.1)
+                    </div>
+                </div>
+
+            </div>
+            <div class="color-swatches-container">  
+                <div>
+                    <div class="color-swatch indigo-gradient"></div>
+                    <div>
+                        $indigo-gradient<br>
+                        #E6F0FF<br>
+                        hsl(214, 100%, 95%)
+                    </div>
+                </div>
+                <!-- ... (similar changes for gradient shades) ... -->
             </div>
         </div>
-        <div class="color-swatches-container">  
-            <div>
-                <div class="color-swatch blue-gradient"></div>
-                <div>
-                    $blue-gradient<br>
-                    #E6F0FF<br>
-                    hsl(214, 100%, 95%)
-                </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-gradient-start"></div>
-                <div>
-                    $blue-gradient-start<br>
-                    #E6F0FF<br>
-                    hsl(214, 100%, 95%)
-                </div>
-            </div>
-            <div>
-                <div class="color-swatch blue-gradient-end"></div>
-                <div>
-                    $blue-gradient-end<br>
-                    #E6F0FF<br>
-                    hsl(214, 100%, 95%)
-                </div>
-            </div>
-        </div>
-    </div>
+    </section>
+
 </section>
