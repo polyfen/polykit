853f9f48907564276b97d3fa268d8b9480998676
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-04 23:17:51 +0100

    Update _intro.php

--- includes/content/home/_intro.php
+++ includes/content/home/_intro.php
@@ -2,7 +2,6 @@
     <h2 class="heading-1 align-center">We ship CSS and Figma updates <strong>together</strong></h2>
     <div class="two-col">
         <div>
-            <p style="margin-top: 1rem; margin-bottom: 0;">The code is</p>
             <ul>
                 <li>Light-weight</li>
                 <li>Mobile-first</li>
@@ -11,13 +10,12 @@
                 <li>Accessability best practices</li>
                 <li>SEO best practices</li>
                 <li>Easy to read</li>
-                <li>Easy to pick-up</li>
-                <li>Easy to maintain and scale</li>
             </ul>
         </div>
         <div>
-            <p style="margin-top: 1rem; margin-bottom: 0;">The design is</p>
             <ul>
+                <li>Easy to pick-up</li>
+                <li>Easy to maintain and scale</li>
                 <li>Clean</li>
                 <li>Consistent</li>
                 <li>Typography best practices</li>
