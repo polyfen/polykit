be7a818bcd668ec12ffd25c2adcc96033d96014a
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-05 01:21:23 +0100

    Update _hellobar.php

--- includes/_hellobar.php
+++ includes/_hellobar.php
@@ -3,10 +3,8 @@
         <small>v 0.1</small>
         <a href="#"><small>Changelog</small></a>
         <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>Roadmap</small></a>
-        <span style="opacity:0.2;">|</span>
-        <small>Show your support</small>
         <a href="https://forms.gle/2GauXepH1gnjCiUh8" target="_blank"><small>Volunteer</small></a>
         <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>Donate</small></a>
-        <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>Newsletter</small></a>
+        <a href="#" target="_blank"><small>Newsletter</small></a>
     </div>
 </section>
