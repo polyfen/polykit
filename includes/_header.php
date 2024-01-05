35b93325cc63b39803a4e0ea7a7909a518f90bda
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-05 01:20:14 +0100

    Hellobar update

--- includes/_header.php
+++ includes/_header.php
@@ -2,11 +2,6 @@
     <a href="/">
         <img src="imgs/polykit-logo.svg" height="50px" width="auto">
     </a>
-    <div style="display:flex; gap:8px;">
-        <small>v 0.1</small>
-        <a href="#"><small>Changelog</small></a>
-        <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>Roadmap</small></a>
-    </div>
     <nav>
         <a href="/">Home</a>
         <a href="getting-started.php">Getting Started</a>
--- includes/_hellobar.php
+++ includes/_hellobar.php
@@ -1,5 +1,9 @@
 <section class="hellobar">
     <div class="container align-center" style="display: flex;">
+        <small>v 0.1</small>
+        <a href="#"><small>Changelog</small></a>
+        <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>Roadmap</small></a>
+        <span style="opacity:0.2;">|</span>
         <small>Show your support</small>
         <a href="https://forms.gle/2GauXepH1gnjCiUh8" target="_blank"><small>Volunteer</small></a>
         <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>Donate</small></a>
