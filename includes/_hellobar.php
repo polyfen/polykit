02e6fdb3c889a2ceb1055ed805208cb42c0aa649
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-05 21:49:43 +0100

    Update _hellobar.php

--- includes/_hellobar.php
+++ includes/_hellobar.php
@@ -1,10 +1,10 @@
 <section class="hellobar">
     <div class="container align-center" style="display: flex;">
         <small>v 0.1</small>
-        <a href="#"><small>Changelog</small></a>
-        <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>Roadmap</small></a>
-        <a href="https://forms.gle/2GauXepH1gnjCiUh8" target="_blank"><small>Volunteer</small></a>
-        <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>Donate</small></a>
-        <a href="#" target="_blank"><small>Newsletter</small></a>
+        <a href="#"><small>🤖 Changelog</small></a>
+        <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>🎯 Roadmap</small></a>
+        <a href="https://forms.gle/2GauXepH1gnjCiUh8" target="_blank"><small>🙋‍♂️ Volunteer</small></a>
+        <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>💝 Donate</small></a>
+        <a href="#" target="_blank"><small>📬 Newsletter</small></a>
     </div>
 </section>
