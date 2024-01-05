225c4fa661bb52d27fa4b1e58624df5648201407
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-05 01:16:40 +0100

    Update _hellobar.php

--- includes/_hellobar.php
+++ includes/_hellobar.php
@@ -1,6 +1,6 @@
 <section class="hellobar">
     <div class="container align-center" style="display: flex;">
-    Show your support
+        <small>Show your support</small>
         <a href="https://forms.gle/2GauXepH1gnjCiUh8" target="_blank"><small>Volunteer</small></a>
         <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>Donate</small></a>
         <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>Newsletter</small></a>
