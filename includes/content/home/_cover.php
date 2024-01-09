52222e40cf4604a2b0b65ab51c95bcc5513aba79
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-09 21:10:49 +0100

    Update _cover.php

--- includes/content/home/_cover.php
+++ includes/content/home/_cover.php
@@ -1,4 +1,4 @@
-<section class="align-center container">
+<section class="align-center">
     <h1 class="title-1 align-center">An Open-source Design System</h1>
 
     <p class="align-center">Made with a focus on beautiful typography and seamless integration between design and code.</p>
