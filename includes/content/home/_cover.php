9f5c40abb0ea8cca5f8ddc9622e59daf42f10e22
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-01-09 21:17:00 +0100

    Update _cover.php

--- includes/content/home/_cover.php
+++ includes/content/home/_cover.php
@@ -1,7 +1,7 @@
 <section class="align-center">
     <h1 class="title-1 align-center">Open-source Design System</h1>
 
-    <p class="align-center" style="max-width:400px;">Made with a focus on beautiful typography and seamless integration between design and code.</p>
+    <p class="align-center" style="max-width:400px; margin: 1rem auto 3rem;">Made with a focus on beautiful typography and seamless integration between design and code.</p>
     
     <a class="button" href="https://github.com/polyfen/polykit" target="_blank"><i class="icon ph-fill ph-github-logo" style="margin-right:0.375rem;"></i> Github Repo</a>
     <a class="button" href="https://www.figma.com/file/ljE12vilrDMg0r9vJfQLBz/Polykit-%7C-Design-System-%5Bv0.1%5D-%5BSTG%5D?type=design&node-id=84%3A51&mode=design&t=sdkwx6zC4JQrObhN-1" target="_blank"><i class="icon ph-fill ph-figma-logo" style="margin-right:0.375rem;"></i> Figma File</a>
