4646e735f99c8078d591a4c5eedf896cb9e21016
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-03-29 20:16:44 +0100

    add: changelog link in hellobar

--- includes/_hellobar.php
+++ includes/_hellobar.php
@@ -1,7 +1,7 @@
 <section class="hellobar">
     <div class="container align-center" style="display: flex;">
         <small>v 0.1</small>
-        <a href="#"><small>🤖 Changelog</small></a>
+        <a href="https://log.thepolyfengroup.com/category/plkt/" target="_blank"><small>🚀 Changelog</small></a>
         <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>🎯 Roadmap</small></a>
         <a href="https://forms.gle/2GauXepH1gnjCiUh8" target="_blank"><small>🙋‍♂️ Volunteer</small></a>
         <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>💝 Donate</small></a>
