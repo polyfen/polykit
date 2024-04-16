573604b1fd0484ec78042757000284d36218bac6
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-04-16 20:50:48 +0200

    improve: home page design

--- includes/content/home/_atomic-design.php
+++ includes/content/home/_atomic-design.php
@@ -1,5 +1,5 @@
 <section>
     <h2 class="heading-1 align-center">Built with Atomic Design</h2>
-    <p class="align-center">Our design system follows the rationale proposed by Brad Frost in a blog post called <a href="https://bradfrost.com/blog/post/atomic-web-design/">&quot;Atomic Web Design&quot;</a>.</p>
+    <p class="align-center" style="max-width: 500px;margin-left:auto;margin-right:auto;">Our design system follows the rationale proposed by Brad Frost in a blog post called <a href="https://bradfrost.com/blog/post/atomic-web-design/">&quot;Atomic Web Design&quot;</a>.</p>
     <img src="imgs/site/atomic-design.png" alt="Atomic Design" width="100%">
 </section>
--- includes/content/home/_wordpress-themes.php
+++ includes/content/home/_wordpress-themes.php
@@ -1,4 +1,4 @@
 <section>
-    <h2 class="heading-1 align-center">Themes for WordPress &amp; Jekyll</h2>
+    <h2 class="heading-1 align-center">Themes for WordPress</h2>
     <h2 class="caption align-center uppercase">Coming Soon</h2>
 </section>
