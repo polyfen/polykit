d08d31f6618e3a410796176f8bccf890cf68ea54
Sebastian Bocchio Oguic <sbocchio@gmail.com>
2024-05-20 02:00:19 +0200

    overflow-getting-started
    Add overflow attr to <pre> tag
    Add button to copy link content

--- includes/content/getting-started/_install-css.php
+++ includes/content/getting-started/_install-css.php
@@ -1,6 +1,11 @@
+<script language="javascript" type="text/javascript">
+    function copiarLink(){
+        navigator.clipboard.writeText(document.getElementById("cssLink").textContent);
+    }
+</script>
 <section>
     <h3 class="heading-2">Installing Polykit via CSS</h2>
     <p>Add the following line of code in your <code>&lt;head&gt;</code>.</p>
-    <pre><code>&lt;link <span class="hljs-keyword">rel</span>=<span class="hljs-string">"stylesheet"</span> href=<span class="hljs-string">"https://polykit.xyz/app/css/polykit-v0-1.css"</span> <span class="hljs-keyword">type</span>=<span class="hljs-string">"text/css"</span>&gt;
-</code></pre>
+    <pre><code id="cssLink">&lt;link <span class="hljs-keyword">rel</span>=<span class="hljs-string">"stylesheet"</span> href=<span class="hljs-string">"https://polykit.xyz/app/css/polykit-v0-1.css"</span> <span class="hljs-keyword">type</span>=<span class="hljs-string">"text/css"</span>&gt;</code></pre>
+<button onClick="javascript:copiarLink();">Copiar Link</button>
 </section>
