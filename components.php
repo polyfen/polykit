fc470fa3062f3c860a88a6eda0105ec0e355d938
xxiicode <leandrobilokapic@gmail.com>
2024-06-05 16:19:18 +0200

    button component 1

--- components.php
+++ components.php
@@ -54,6 +54,9 @@
     <!-- PAGE-SPECIFIC META start -->
     <!-- PAGE-SPECIFIC META end -->
 
+        <!-- CSS extraoficial -->
+        <link rel="stylesheet" href="/app/css/extra.css" type="text/css">
+    </head>
 
 
 </head>
@@ -73,11 +76,32 @@
         <ul>
             <li><a href="#section_subatomics">Subatomics</a></li>
             <ul>
-                <li><a href="#section_branding">Branding</a></li>
-                <li><a href="#section_grid">Grid</a></li>
-                <li><a href="#section_typography">Typography</a></li>
-                <li><a href="#section_colors">Colors</a></li>
-                <li><a href="#section_modes">Modes</a></li>
+                <li><a href="#">Subatomics</a></li>
+                <ul>
+                    <li><a href="#">Branding</a></li>
+                    <li><a href="#">Grid</a></li>
+                    <li><a href="#">Typography</a></li>
+                    <li><a href="#">Colors</a></li>
+                    <li><a href="#">Modes</a></li>
+                </ul>
+                <li><a href="#">Atoms</a></li>
+                <ul>
+                    <li><a href="#">Containers</a></li>
+                    <li><a href="#">Text</a></li>
+                    <li><a href="#">Icons</a></li>
+                    <li><a href="#">Lists</a></li>
+                    <li><a href="#buttons">Buttons</a></li>
+                    <li><a href="#">Separators</a></li>
+                    <li><a href="#">Codeblocks</a></li>
+                </ul>
+                <li><a href="#">Molecules</a></li>
+                <ul>
+                    <li><a href="#">Nav</a></li>
+                </ul>
+                <li><a href="#">Organisms</a></li>
+                <ul>
+                    <li><a href="#">Header</a></li>
+                </ul>
             </ul>
             <li><a href="#section_atoms">Atoms</a></li>
             <ul>
