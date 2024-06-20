3945899b6c4dcaac90bab5b4fadd64a8b9d9620c
xxiicode <leandrobilokapic@gmail.com>
2024-06-20 17:33:45 +0200

    a

--- includes/content/components/_buttons.php
+++ includes/content/components/_buttons.php
@@ -18,10 +18,10 @@
             <button class="tab-button small-button" data-tab="html">HTML</button>
             <button class="tab-button small-button" data-tab="css">CSS</button>
         </div>
-        <pre class="tab-panel html-content active"><code>&lt;button&gt;algo&lt;/button&gt;
-&lt;button&gt;algo2&lt;/button&gt;
-&lt;button&gt;algo3&lt;/button&gt;</code></pre>
-        <pre class="tab-panel css-content"><code>button, a.button {
+        <pre class="tab-panel html-content active"><code>&lt;button class="button"&gt;&lt;/button&gt;
+&lt;button class="button secondary-button"&gt;&lt;/button&gt;
+&lt;button class="buton cta-button&gt;&lt;/button&gt;</code></pre>
+        <pre class="tab-panel css-content"><code>.button {
     background: $accent-color;
     color: $white;
     padding: 0.75rem 1.5rem;
@@ -37,7 +37,21 @@
     align-items: center;
     margin: 1rem auto;
     cursor: pointer;
-}</code></pre>
+}
+.secondary-button {
+    background-color: $indigo900;
+    box-shadow: 0 0 4px hsl(208, 100%, 53%),
+    inset 0 0 0 4px hsla(234, 61%, 6%, 0.6);
+}
+
+.cta-button {
+    background-image: linear-gradient(45deg,
+        hsl(228, 100%, 53%),
+        hsl(188, 100%, 53%));
+    background-size: 200% 200%;
+    animation: gradient 7s infinite;
+}
+</code></pre>
     </section>
 
     <h4 class="heading-3">Size</h4>
@@ -58,26 +72,29 @@
             <button class="tab-button small-button" data-tab="html">HTML</button>
             <button class="tab-button small-button" data-tab="css">CSS</button>
         </div>
-        <pre class="tab-panel html-content active"><code>&lt;button&gt;algo&lt;/button&gt;
-&lt;button&gt;algo2&lt;/button&gt;
-&lt;button&gt;algo3&lt;/button&gt;</code></pre>
-        <pre class="tab-panel css-content"><code>button, a.button {
-    background: $accent-color;
-    color: $white;
-    padding: 0.75rem 1.5rem;
-    border-radius: 10rem;
-    text-decoration: none;
-    letter-spacing: 0.5px;
-    font-weight: 600;
-    box-shadow: 0 0 2px $accent-color;
-    display: flex;
-    width: 240px;
-    max-width: 100%;
-    justify-content: center;
-    align-items: center;
-    margin: 1rem auto;
-    cursor: pointer;
-}</code></pre>
+        <pre class="tab-panel html-content active"><code>&lt;button class="button small-button"&gt;&lt;/button&gt;
+&lt;button class="button"&gt;&lt;/button&gt;
+&lt;button class="button large-button"&gt;&lt;/button&gt;</code></pre>
+        <pre class="tab-panel css-content"><code>.small-button {
+    height: 35px;
+    font-size: 0.75rem;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    padding: 0.5rem 1rem;
+    width: 180px;
+}
+
+(regularbutton)
+
+large-button {
+    padding: 1.25rem 1rem;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    width: 260px;
+}
+
+
+</code></pre>
     </section>
 
     <h4 class="heading-3">Icons</h4>
@@ -98,25 +115,79 @@
             <button class="tab-button small-button" data-tab="html">HTML</button>
             <button class="tab-button small-button" data-tab="css">CSS</button>
         </div>
-        <pre class="tab-panel html-content active"><code>&lt;button&gt;algo&lt;/button&gt;
-&lt;button&gt;algo2&lt;/button&gt;
-&lt;button&gt;algo3&lt;/button&gt;</code></pre>
-        <pre class="tab-panel css-content"><code>button, a.button {
-    background: $accent-color;
-    color: $white;
-    padding: 0.75rem 1.5rem;
-    border-radius: 10rem;
-    text-decoration: none;
-    letter-spacing: 0.5px;
-    font-weight: 600;
-    box-shadow: 0 0 2px $accent-color;
-    display: flex;
-    width: 240px;
-    max-width: 100%;
-    justify-content: center;
-    align-items: center;
-    margin: 1rem auto;
-    cursor: pointer;
-}</code></pre>
+        <pre class="tab-panel html-content active"><code>&lt;button data-button-icon="phone"&gt;&lt;/button&gt;
+&lt;button data-button-arrow&gt;&lt;/button&gt;
+&lt;button data-button-arrow-up&gt;&lt;/button&gt;</code></pre>
+        <pre class="tab-panel css-content"><code>[data-button-icon]::before {
+    content: '';
+    display: inline-block;
+    width: 30px;
+    height: 30px;
+    margin-right: 8px;
+    vertical-align: middle;
+    background-repeat: no-repeat;
+    background-position: center center;
+}
+
+[data-button-icon="phone"]::before {
+    background-image: url(../../imgs/icons/phone-call.svg);
+    filter:none;
+    margin-block: -8px;
+}
+
+[data-button-arrow]::after {
+    content: '';
+    background-image: url(/imgs/icons/arrow-right-fill.svg);
+    background-repeat: no-repeat;
+    display: inline-block;
+    opacity: .8;
+    height: 10px;
+    width: 18px;
+    margin-left: 6px;
+}
+
+@keyframes moveArrow {
+    0% {
+        transform: translateX(0);
+    }
+    70% {
+        transform: translateX(10px);
+    }
+    100% {
+        transform: translateX(10px);
+    }
+}
+
+[data-button-arrow-up]::before,
+[data-button-arrow-up]::after {
+    content: url(/imgs/icons/arrow-up-fill.svg);
+    display: inline-block;
+    opacity: .8;
+    animation: moveArrow-up 1s ease-out infinite;
+    height: 16px;
+    width: 9px;
+}
+
+[data-button-arrow-up]::before {
+    margin-right: 10px;
+}
+
+[data-button-arrow-up]::after {
+    margin-left: 10px;
+}
+
+@keyframes moveArrow-up {
+    0% {
+        transform: translatey(0px);
+    }
+    70% {
+        transform: translatey(-5px);
+    }
+    100% {
+        transform: translatey(-5px);
+    }
+}
+
+</code></pre>
     </section>
 </section>
