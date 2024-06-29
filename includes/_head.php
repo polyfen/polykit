68181a76063a7e94b84d6145dc8db389e3d6bbf5
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-06-30 01:01:45 +0200

    #

--- includes/_head.php
+++ includes/_head.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="app/css/polykit-v0-3.css" type="text/css">
 
 <!-- Scripts -->
-<script defer src="app/js/navbar.js"></script>
+<script defer src="app/js/polykit-v0-3.js"></script>
 <script>
 document.addEventListener("DOMContentLoaded", function() {
     let currentPage = window.location.pathname;
