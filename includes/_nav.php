1fbd383b38f38984fddd81fb73cc3c985fa16ccd
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-07-01 08:20:18 +0200

    #

--- includes/_nav.php
+++ includes/_nav.php
@@ -1,9 +1,9 @@
 <nav>
-    <div class="nav-links" id="nav-links">
+    <div class="nav-links">
         <a href="/">Home</a>
         <a href="/docs.php">Docs</a>
     </div>
-    <div class="nav-buttons" id="nav-buttons">
+    <div class="nav-buttons">
         <button aria-label="Open/Close navbar" class="nav-hamburger">
             <img src="imgs/hamburger_icon.svg" alt="" height="30">
         </button>
