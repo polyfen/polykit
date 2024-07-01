92becb4484a68f52cd58d5ddfa18e095810d21cf
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-07-01 19:30:53 +0200

    #

--- includes/content/home/_intro.php
+++ includes/content/home/_intro.php
@@ -1,7 +1,7 @@
 <section class="product-features">
     <h2 class="heading-1 align-center">We ship CSS and Figma updates <strong>together</strong></h2>
-    <div class="two-col">
-        <div>
+    <div class="row">
+        <div class="col">
             <ul class="hanging-list arrow-bullets">
                 <li>Light-weight</li>
                 <li>Mobile-first</li>
@@ -12,7 +12,7 @@
                 <li>Easy to read</li>
             </ul>
         </div>
-        <div>
+        <div class="col">
             <ul class="hanging-list arrow-bullets">
                 <li>Easy to pick-up</li>
                 <li>Easy to maintain and scale</li>
