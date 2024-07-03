6b48fe49bb3dc5a34f125c8267c87fcaad65434e
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-07-03 10:26:38 +0200

    #

--- includes/_hellobar.php
+++ includes/_hellobar.php
@@ -1,10 +1,6 @@
 <section class="hellobar">
     <div class="container align-center">
-        <small>v 0.3</small>
-        <a href="https://log.thepolyfengroup.com/category/plkt/" target="_blank"><small>🚀 Changelog</small></a>
-        <a href="https://trello.com/b/QXjZGW58/polykit-product-roadmap" target="_blank"><small>🎯 Roadmap</small></a>
-        <a href="https://forms.gle/2GauXepH1gnjCiUh8" target="_blank"><small>🙋‍♂️ Volunteer</small></a>
-        <a href="https://donate.stripe.com/8wM3ebboP7n79i0cMM" target="_blank"><small>💝 Donate</small></a>
-        <a href="#" target="_blank"><small>📬 Newsletter</small></a>
+        <small style="margin-right: 6px;">v 0.3</small>
+        <a href="https://log.thepolyfengroup.com/category/plkt/" target="_blank"><small>Changelog</small></a>
     </div>
 </section>
