c462e236492d438430dfb5caf20a7c407f579bf4
Boris Hrnčić <borishrncic@users.noreply.github.com>
2024-07-05 00:14:23 +0200

    #

--- app/js/polykit-v0-3.js
+++ app/js/polykit-v0-3.js
@@ -66,20 +66,3 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 /* End Current Page */
-
-
-/* Accordions */
-
-let acc = document.getElementsByClassName("acc-button");
-let i;
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
-    });
-}
